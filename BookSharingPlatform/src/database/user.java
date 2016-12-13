7e5f9707b162c95e05f11794fb41d148f9bd36e7
dilarakorkmaz <dilarakorkmaz@192.168.1.30>
2016-12-13 23:23:48 +0200

    testing

--- BookSharingPlatform/src/database/user.java
+++ BookSharingPlatform/src/database/user.java
@@ -7,46 +7,48 @@ import java.sql.Statement;
 import javax.swing.JOptionPane;
 
 public class user {
-
-	private int user_ID; 
-	private String username;
-	private String password;
-
-	public int getUserId(){
-		return user_ID;	
-	}
-	public String getUserName(){
-		return username;	
-	}
-	public String getPassword(){
-		return password;	
-	}
-
-	public int displayUser1(String username,String password ) throws SQLException,ClassNotFoundException{
-
-		try {
-			String queryString = "SELECT 'username','password' FROM 'user'";
-			ResultSet results =null; // Statement.executeQuery(queryString);
-
-			while (results.next()) {
-				username = results.getString(queryString);
-				password =  results.getString(queryString);
-
-				if ((username.equals(username)) && (password.equals(password))) {
-
-					JOptionPane.showMessageDialog(null, "Username and Password exist");  
-				}else {
-
-					JOptionPane.showMessageDialog(null, "Please Check Username and Password ");
-				}
-				results.close();
-			} 
-		}  
-		catch (SQLException sql) {
-
-			System.out.println(sql);
+	
+	public int user_ID; 
+	public String username;
+	public String password;
 
 
-		}
-		return 0;//////////////Silinicek
-	}}
+
+public int getUserId(){
+	return user_ID;	
+}
+public String getUserName(){
+	return username;	
+}
+public String getPassword(){
+	return password;	
+}
+
+public int displayUser1(String username,String password ) throws SQLException,ClassNotFoundException{
+	
+	try {
+        String queryString = "SELECT 'username','password' FROM 'user'";
+        ResultSet results = Statement.executeQuery(queryString);
+
+        while (results.next()) {
+    username = results.getString();
+      password =  results.getString();
+
+           if ((username.equals(username)) && (password.equals(password))) {
+
+              JOptionPane.showMessageDialog(null, "Username and Password exist");  
+        }else {
+
+         JOptionPane.showMessageDialog(null, "Please Check Username and Password ");
+        }
+        results.close();
+    } 
+	}  
+        catch (SQLException sql) {
+
+        out.println(sql);
+	
+	
+}
+    	
+}
